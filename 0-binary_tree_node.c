e8cca9b1eed170f92288301ae7d97ffe904a4628
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-04 05:26:11 -0700

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function
+ * @parent: paremter
+ * @value: parameter
+ * Return: binary_tree_t
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
+
+	if (tmp == NULL)
+		return NULL;
+	else
+	{
+		if (parent == NULL)
+			tmp->parent = parent;
+		else
+		{
+			tmp->parent = parent;
+		}
+		tmp->n = value;
+		tmp->right = NULL;
+		tmp->left = NULL;
+		return (tmp);
+	}
+}
