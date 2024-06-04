eb026f18e9c21510d1096a5aa6eb55ff108bc8f3
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-04 05:32:10 -0700

    don't use else after return

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,9 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tmp = malloc(sizeof(binary_tree_t));
 
-	if (tmp == NULL)
-		return NULL;
-	else
+	if (tmp != NULL)
 	{
 		if (parent == NULL)
 			tmp->parent = parent;
@@ -22,6 +20,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 			tmp->parent = parent;
 		}
 		tmp->n = value;
+		tmp->right = NULL;
+		tmp->left = NULL;
 		return (tmp);
 	}
+	else
+		return (NULL);
 }
