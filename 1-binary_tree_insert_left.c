270034a5e29f94592e18658be3292ecb0a9d6e86
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-05 01:53:56 -0700

    first commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		binary_tree_t *temp = malloc(sizeof(binary_tree_t));
 
-		temp->n = parent->left->n;
+		temp->n = parent->left;
 		parent->left = tmp;	
 		temp->parent = tmp;
 		temp->right = NULL;
