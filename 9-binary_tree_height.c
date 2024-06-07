b71a12e96385a1457d625d9aae6c520d7c31fd6b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-06-06 17:10:26 -0700

    betty-style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	lft = tree->left ? 1 + binary_tree_height(tree->left): 0;
+	lft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	rght = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (lft > rght ? lft : rght);
 }
